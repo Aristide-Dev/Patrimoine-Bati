25b763846b8cdad26faae1a34e2729bea53df82a
AristechDev <aristidegnimassouu@gmail.com>
2025-01-23 11:02:24 +0000

    change many images

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -489,7 +489,7 @@ export default function Welcome() {
       {/* <a href="/">
           <img src="/images/logo/Armoiries-Republique-de-Guinee-V2.png" alt="Logo de la MAMRI" className="w-1/2 md:w-1/4 mx-auto" />
         </a> */}
-        <img src="/images/logo/Armoiries-Republique-de-Guinee-V2.png" alt="Logo de la MAMRI" className="w-1/2 md:w-1/4 mx-auto" />
+        <img src="/images/logo/Armoiries-Republique-de-Guinee-V2.png" alt="Logo de la MAMRI" className="w-1/2 rounded-full md:w-1/4 mx-auto bg-white" />
         <div className="container mx-auto px-4 text-center">
           <h2 className="text-4xl font-bold text-white mb-6">
             Ensemble, construisons l'avenir de la Guinée
